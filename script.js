dbc553cbbaff116fe99f2ac231e35561cd0bd722
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-13 16:58:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,21 +12,24 @@ function mincost(arr)
 	return mincost;
 }
 function twoSmallest(arr) {
-	let min1 = Infinity;
-	let min2 = Infinity;
-	for(const num of arr)
-		{
-			if(num<min1)
-			{
-				min2 = min1;
-				min1 = num;
-			}
-			else if(num<min2)
-			{
-				min2 = num;
-			}
-		}
-	return [min1,min2];
+  if (arr.length < 2) {
+    throw new Error("Array must have at least two elements");
+  }
+
+  let min1 = arr[0];
+  let min2 = arr[1];
+
+  for (let i = 1; i < arr.length; i++) {
+    const num = arr[i];
+
+    if (num !== min1) {
+      min2 = num;
+      break;
+    }
+  }
+
+  return [min1, min2];
 }
+
 
 module.exports=mincost;
