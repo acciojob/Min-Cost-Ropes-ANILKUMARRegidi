7b6687ac15f7a8d0dd4d76fd011f2d4da1a3a10e
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-13 16:50:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+  let mincost = 0;
+	while(arr.length>1)
+		{
+			const[min1,min2] = twoSmallest(arr);
+			let sum = min1 + min2;
+			mincost += sum;
+			arr.push(sum);
+			arr = arr.filter((e) => e!==min1 && e!==min2)
+		}
+	return mincost;
+}
+function twoSmallest(arr) {
+	let min1 = Infinity;
+	let min2 = Infinity;
+	for(const num of arr)
+		{
+			if(num<min1)
+			{
+				min2 = min1;
+				min1 = num;
+			}
+			else if(num<min2)
+			{
+				min2 = num;
+			}
+		}
+	return [min1,min2];
 }
 
 module.exports=mincost;
